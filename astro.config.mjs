169d275b3f127777cb449ee19194d183e00e6d40
Accio Operator <worker@accio.ai>
2026-05-13 11:53:36 +0800

    chore: trigger fresh build with Node 20 environment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,4 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  image: {
-    service: { entrypoint: 'astro/assets/services/noop' }
-  }
 });
